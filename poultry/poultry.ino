d2b4df5421861fae123c992cad4898916214732d
lindsaylillian <150136754+lindsaylillian@users.noreply.github.com>
2024-07-11 15:18:26 +0300

    made changes to lcd

--- poultry/poultry.ino
+++ poultry/poultry.ino
@@ -1,4 +1,6 @@
 #include <HX711.h>
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(19,23,18,17,16,15);
 
 const int LOADCELL_DOUT_PIN = 2; // Data output pin
 const int LOADCELL_SCK_PIN = 3;  // Clock pin
@@ -7,24 +9,33 @@ HX711 scale;
 
 void setup() {
   Serial.begin(115200); // Initialize serial communication at 115200 baud
+  lcd.begin(16,2);
   scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN); // Initialize the scale with the specified pins
-  Serial.println("Tare - Remove weights"); // Prompt to remove weights
+  lcd.print("Tare - Remove weights"); // Prompt to remove weights
+  lcd.setCursor(0,1);
   delay(5000); // Wait for 5 seconds
   scale.tare(); // Reset the scale to 0
-  Serial.println("Tare done...");
-  Serial.println("Now put weight on the scale"); // Prompt to put weights
+  lcd.clear();
+  lcd.print("Tare done...");
+  delay(2000);
+  lcd.clear();
+  lcd.print("Now put weight on the scale"); // Prompt to put weights
+  lcd.setCursor(0,1);
+  lcd.print("on the scale");
   delay(5000); // Wait for 5 seconds
 }
 
 void loop() {
+  
   if (scale.is_ready()) {
     long reading = scale.get_units(10); // Get average of 10 readings to calculate weight
     float weight = reading / 419.8; // Divide by calibration factor to get the weight in desired units
-    Serial.print("Weight: ");
-    Serial.print(weight, 2); // Print the weight with 2 decimal places
-    Serial.println(" kg");
+    lcd.clear();
+    lcd.print("Weight: ");
+    lcd.print(weight, 2); // Print the weight with 2 decimal places
+    lcd.print(" kg");
   } else {
-    Serial.println("HX711 not found.");
+    lcd.print("HX711 not found.");
   }
   delay(1000); // Delay before the next reading
 }
