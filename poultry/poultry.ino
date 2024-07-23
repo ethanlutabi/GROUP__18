b3cf2066962b2d784aa32579689072b1fab12f4a
gachiza <157603855+gachiza@users.noreply.github.com>
2024-07-24 01:18:56 +0300

    function for lcd

--- poultry/poultry.ino
+++ poultry/poultry.ino
@@ -57,31 +57,6 @@ bool lastSteadyPressureCounted = false; // Flag to track last steady pressure
 
 
 
-const int LOADCELL_DOUT_PIN = 2; // Data output pin
-const int LOADCELL_SCK_PIN = 3;  // Clock pin
-
-HX711 scale;
-
-void setup() {
-  Serial.begin(115200); // Initialize serial communication at 115200 baud
-  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN); // Initialize the scale with the specified pins
-  Serial.println("Tare - Remove weights"); // Prompt to remove weights
-  delay(5000); // Wait for 5 seconds
-  scale.tare(); // Reset the scale to 0
-  Serial.println("Tare done...");
-  Serial.println("Now put weight on the scale"); // Prompt to put weights
-  delay(5000); // Wait for 5 seconds
-}
-
 void loop() {
-  if (scale.is_ready()) {
-    long reading = scale.get_units(10); // Get average of 10 readings to calculate weight
-    float weight = reading / 419.8; // Divide by calibration factor to get the weight in desired units
-    Serial.print("Weight: ");
-    Serial.print(weight, 2); // Print the weight with 2 decimal places
-    Serial.println(" kg");
-  } else {
-    Serial.println("HX711 not found.");
-  }
-  delay(1000); // Delay before the next reading
-}
+
+
