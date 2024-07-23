68b67a1930e8f86fda0f9293cffc0b111a661471
Twongiirwe <twongiirwesusan@gmail.com>
2024-07-24 02:27:12 +0300

    keypad input

--- poultry/poultry.ino
+++ poultry/poultry.ino
@@ -111,3 +111,12 @@ void loop() {
   char key = keypad.getKey();
   if (key){
     Serial.println(key);
+
+     if (settingWeight) {
+      if (key == 'D') {
+        // Finish weight input
+        if (settingMode == 1) {
+          weightThreshold1 = weightInput.toFloat();
+          Serial.print("New minimum weight: ");
+          Serial.println(weightThreshold1);
+
