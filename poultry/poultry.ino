f3e3b1e4af8b1d54a87aa13baa78d1128dfc5e35
Twongiirwe <twongiirwesusan@gmail.com>
2024-07-24 02:30:28 +0300

    lcd display based on user input

--- poultry/poultry.ino
+++ poultry/poultry.ino
@@ -119,4 +119,15 @@ void loop() {
           weightThreshold1 = weightInput.toFloat();
           Serial.print("New minimum weight: ");
           Serial.println(weightThreshold1);
+lcd.clear();
+          lcd.setCursor(0, 0);
+          lcd.print("Min weight set");
+        } else if (settingMode == 2) {
+          weightThreshold2 = weightInput.toFloat();
+          Serial.print("New average weight: ");
+          Serial.println(weightThreshold2);
+          lcd.clear();
+          lcd.setCursor(0, 0);
+          lcd.print("Avg weight set");
+        }
 
