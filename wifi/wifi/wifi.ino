ec5dae34058756cbb2e12f8d97437da8a9496da3
Twongiirwe <twongiirwesusan@gmail.com>
2024-07-24 22:56:39 +0300

    setting weight

--- wifi/wifi/wifi.ino
+++ wifi/wifi/wifi.ino
@@ -3,3 +3,10 @@ void loop() {
   if (key){
     Serial.println(key);
 
+if (settingWeight) {
+      if (key == 'D') {
+        // Finish weight input
+        if (settingMode == 1) {
+          weightThreshold1 = weightInput.toFloat();
+          Serial.print("New minimum weight: ");
+          Serial.println(weightThreshold1);
