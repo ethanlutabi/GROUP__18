4a363171ad2716d9febde80cf162bc8a31ee75f4
ethanlutabi <157450838+ethanlutabi@users.noreply.github.com>
2024-07-25 13:20:47 +0300

    Update door.ino

--- door/door.ino
+++ door/door.ino
@@ -97,3 +97,6 @@ void setup() {
   pinMode(BUTTON_PIN, INPUT_PULLUP); // Use internal pull-up resistor
 
   Wire.begin(8, 9); // Set I2C pins for ESP32-S2 Mini (SDA, SCL)
+
+  lcd.begin(16, 2); // Initialize the LCD
+  lcd.backlight(); // Turn on the LCD backlight
