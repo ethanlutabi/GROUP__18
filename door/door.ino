8902711f3bc24ef68b07dfc4ef22ce0474b97c3b
lindsaylillian <150136754+lindsaylillian@users.noreply.github.com>
2024-07-25 10:03:59 +0300

    Update door.ino

--- door/door.ino
+++ door/door.ino
@@ -122,6 +122,13 @@ void loop(){
   if (settingWeight){
     if (key == 'D'){
       //finish weight input
-      if (settingMode == 1)
+      if (settingMode == 1){
+        weightThreshold1 = weightInput.toFloat();
+        Serial.print("New minimum weight: ");
+        Serial.println(weightThreshold1);
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print("Avg weight set ");
+        
 
 
