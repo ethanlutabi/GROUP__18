c17378dc6515d0424b2111e739c59e01003b854d
Ddumba Jonah Brown <154723707+brownjh18@users.noreply.github.com>
2024-07-30 01:04:26 +0300

    Add files via upload

---
+++ Weight_algorithm/Weight_algorithm.ino
@@ -0,0 +1,39 @@
+#include <ESP32Servo.h>
+#include "HX711.h"
+#include <Wire.h>
+#include <Keypad.h>
+#include <LiquidCrystal_I2C.h>
+#include <WiFi.h>
+#include "ThingSpeak.h"
+
+#define SERVO_PIN 4
+#define SECOND_SERVO_PIN 2
+#define RED_LED_PIN 13
+#define GREEN_LED_PIN 12
+#define LOADCELL_DOUT_PIN 18
+#define LOADCELL_SCK_PIN 19
+#define BUTTON_PIN 5
+
+const char* ssid = "Wokwi-GUEST";
+const char* password = "";
+unsigned long myChannelNumber = 2606703;
+const char* myWriteAPIKey = "MEAYCUGPIIB31FF4";
+
+WiFiClient client;
+
+HX711 scale;
+Servo servoMotor;
+Servo secondServoMotor;
+LiquidCrystal_I2C lcd(0x27, 16, 2); // Change the address if needed
+
+float weight;
+bool firstDoorOpen = false;
+bool secondDoorOpen = false;
+bool systemEnabled = false;
+bool lastButtonState = HIGH;
+
+bool settingWeight = false;
+String weightInput = "";
+int settingMode = 0;
+unsigned long menuStartTime = 0;
+bool inMenu = false;
