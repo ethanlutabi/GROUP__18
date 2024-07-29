ec0cc006ed54746b8c1858ecf6bb5d867bd96e99
Ddumba Jonah Brown <154723707+brownjh18@users.noreply.github.com>
2024-07-30 01:14:14 +0300

    Update Weight_algorithm.ino

--- Weight_algorithm/Weight_algorithm.ino
+++ Weight_algorithm/Weight_algorithm.ino
@@ -60,6 +60,7 @@ int secondDoorOpenCount = 0;
 float previousWeight = 0.0;
 
 void setLED(int mode = -1) {
+  // 0 = none, 1 = red-glow, 2 = green-glow
   if (mode >= 0 && mode <= 2) {
     ledMode = mode;
   }
