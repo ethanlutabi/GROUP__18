2673b5e30d0ef94a8457ebe5094950b49a38ae6b
lindsaylillian <150136754+lindsaylillian@users.noreply.github.com>
2024-07-31 11:17:35 +0300

    serial

--- poultry/poultry.ino
+++ poultry/poultry.ino
@@ -124,8 +124,7 @@ void loop() {
     if (settingWeight) {
         if (settingMode == 1) {
           weightThreshold1 = weightInput.toFloat();
-          Serial.print("New minimum weight: ");
-          Serial.println(weightThreshold1);
+          
           lcd.clear();
           lcd.setCursor(0, 0);
           lcd.print("Min weight set");
