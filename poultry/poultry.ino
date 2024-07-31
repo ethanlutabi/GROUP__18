c691ced7d38124b2710295f143203f2da3c08070
lindsaylillian <150136754+lindsaylillian@users.noreply.github.com>
2024-07-31 10:55:35 +0300

    space

--- poultry/poultry.ino
+++ poultry/poultry.ino
@@ -121,7 +121,7 @@ void loop() {
   char key = keypad.getKey();
   if (key){
     Serial.println(key);
-    if (settingWeight) {
+    
         if (settingMode == 1) {
           weightThreshold1 = weightInput.toFloat();
           Serial.print("New minimum weight: ");
